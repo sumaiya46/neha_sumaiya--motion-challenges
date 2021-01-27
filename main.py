639ed4e99c9ffd8ea13c6c22f295888cb7086ad0
sumaiya46 <72760514+sumaiya46@users.noreply.github.com>
2021-01-27 11:18:24 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,16 @@
+#while True:
+ #   print ("Rotation Level:"+ input.rotation(Rotation.PITCH))
+  #  if input.rotation(Rotation.PITCH)>0 or input.rotation(Rotation.PITCH) <0:
+ #       light.show_animation(light.rainbowAnimation, 500)    
+  #  else: 
+  #      music.pew_pew.play_until_done()
+  #      light.set_all(light. rgb (255, 255,0))
+
+#while True:
+  #  print("Acceleration Level:"+ input. acceleration(Dimension.Y))
+ #   if input. acceleration(Dimension.Z) >0 or input. acceleration(Dimension.Z) <0:
+ #       light.set_all(light.rgb(0, 255, 255))
+  #      music.jump_up.play_until_done()
+  #  else:
+  #      light.set_all(light.rgb(255, 255, 255))
+
